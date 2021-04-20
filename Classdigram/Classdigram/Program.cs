f36ba5dfac708fe715e94dadf745fafd0d61afb2
Chowdam Naga Jyothi <chowdam.n.jyothi@intimetec.com>
2021-04-20 09:16:14 +0530

    First commit

---
+++ Classdigram/Classdigram/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Classdigram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+     
+        class AssetInformation_Book
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Year { get; set; }
+        }
+        class AssetHelper : AssetInformation_Book
+        { 
+            public void AddAsset()
+            {
+               
+
+            }
+            public void SearchAsset()
+            {
+
+            }
+            public void UpdateAsset()
+            {
+
+            }
+            public void DeleteAsset()
+            {
+
+            }
+
+            public void DisplayAsset()
+            {
+
+            }
+        }
+
+
+    }
+}
+
